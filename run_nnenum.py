609afed93e42decc999af093cd635ea507be6be0
Rosian Pop <poprosian@gmail.com>
2023-12-09 07:22:34 +0200

    added run nnenum

---
+++ run_nnenum.py
@@ -0,0 +1,28 @@
+import csv
+from pathlib import Path
+import argparse
+import subprocess
+
+
+def run_tool(instances_path: str, script_path: str) -> None:
+    with Path(instances_path).open("r") as f:
+        reader = csv.reader(f)
+        for idx, row in enumerate(reader):
+            onnx, vnnlib, timeout = row
+            subprocess.call(["sh", script_path, onnx, vnnlib, timeout, f"{idx}.txt"])
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--instances", required=True, help="instances.csv file to process."
+    )
+    parser.add_argument(
+        "-s", "--script", required=True, help="Shell script to run the tool."
+    )
+    args = parser.parse_args()
+    run_tool(args.instances, args.script)
+
+
+if __name__ == "__main__":
+    main()
