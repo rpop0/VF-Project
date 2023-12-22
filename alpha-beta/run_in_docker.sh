994c85ef3aa1c80bb193bedab6c6aad5d305d161
Leonard Han <leonnis12@yahoo.com>
2023-12-22 02:01:18 +0200

    Added Alpha Beta scripts and results
    - added both for CPU and CUDA runs

---
+++ alpha-beta/run_in_docker.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# run code in docker. Single argument: space-seperated list of categories
+#
+# example usage: ./run_in_docker.sh "acasxu cifar10_resnet cifar2020 eran marabou-cifar10 mnistfc nn4sys oval21 test verivital"
+
+CATEGORIES="nn4sys"
+
+if [ "$#" -eq 1 ]; then
+    CATEGORIES=$1
+fi
+
+PREFIX=vnncomp2022
+RESULT_FILE=out.csv
+
+CONTAINER=${PREFIX}_container
+IMAGE=${PREFIX}_image
+
+rm -f ${RESULT_FILE}
+echo "Running in Docker using container name $CONTAINER and image name $IMAGE, with categories: $CATEGORIES"
+
+docker kill $CONTAINER
+docker stop $CONTAINER
+docker rm $CONTAINER
+
+docker build --build-arg CATEGORIES="vnncomp2022_container" . -t vnncomp2022_image
+
+docker run -d --privileged --name vnncomp2022_container vnncomp2022_image tail -f 
+#/dev/null
+
+# "docker ps" should now list the image as running
+
+# to get a shell, remove the lines at the end that delete the container and do: "docker exec -it $CONTAINER bash"
+
+
+docker cp vnncomp2022_container:/out.csv out.csv
+
+#docker kill $CONTAINER
+#docker stop $CONTAINER
+#docker rm $CONTAINER
+
+echo "Done. Result file ${RESULT_FILE} should be in the local folder if there were no errors."
